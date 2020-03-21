4810bdbd3b5d036528cc5bc800d6de6f4aa21ca5
Zababurin Sergey <s.zababurin.v@gmail.com>
2020-03-21 04:33:43 +0300

    update

--- docs/static/html/components/component_modules/action/actions-postOffice.mjs
+++ docs/static/html/components/component_modules/action/actions-postOffice.mjs
@@ -1,4 +1,5 @@
 import colorlog from '/static/html/components/component_modules/colorLog/colorLog.mjs'
+
 function bestCopyEver(src) {
     return Object.assign({}, src);
 }
@@ -46,7 +47,42 @@ export default (views,property,color,substrate,relation)=>{
                     }))
 
                 })(views,property,color,substrate,relation)
+                break
+            case 'authtorization':
+                (async (views,property,color,substrate,relation)=>{
 
+                    console.assert(false, substrate)
+               let login =   await fetch("https://api.dellin.ru/v1/customers/login.json", {
+                       method: "POST",
+                       headers: {
+                           'mode': 'no-cors'
+                       },
+                       body: JSON.stringify(substrate)
+                    });
+
+                    login = await login.json()
+
+                    console.assert(false, login)
+
+
+                    // let json = (await Json(views))
+                    // let selected = await json.select(substrate)
+                    // let jsonTemplate = {}
+                    // if(property.hasOwnProperty('property')){
+                    //     jsonTemplate = await json.transformWith(property.property, false, selected)
+                    // }else{
+                    //     jsonTemplate = await json.transformWith(property, false, selected)
+                    // }
+                    // let button = await json.root(jsonTemplate)
+                    // await colorlog(views,'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',color, button, relation )
+                    // document.dispatchEvent( new CustomEvent('actionButton', {
+                    //     detail: {
+                    //         _:'button',
+                    //         button
+                    //     }
+                    // }))
+
+                })(views,property,color,substrate,relation)
                 break
             default:
                 console.warn('actions-postoffice.mjs нет обработчика --->', relation.toLowerCase(), '[(' ,views,property,color,substrate,relation ,')a]')
--- docs/static/html/components/component_modules/action/relation-postOffice.mjs
+++ docs/static/html/components/component_modules/action/relation-postOffice.mjs
@@ -1,20 +1,20 @@
 export default {
     authtorization:[{
-        property:{
+        property:{ },
+        substrate:{
             "appkey": "34A4C6B7-428A-43BC-A193-CBE96F403D7D",
             "login": "s.zababurin.v@gmail.com",
             "password": "fds744502fds"
         },
-        substrate:'default',
     }],
     sendpost:[
         {
-            property:{
+            property:{ },
+            substrate:{
                 "appkey": "34A4C6B7-428A-43BC-A193-CBE96F403D7D",
                 "login": "s.zababurin.v@gmail.com",
                 "password": "fds744502fds"
             },
-            substrate:'default',
 
         }
     ]
